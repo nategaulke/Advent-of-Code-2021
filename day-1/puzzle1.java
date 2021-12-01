96f75c4e14dcade3ad6b6c6ed64233eec5fb1919
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2021-12-01 14:43:12 -0500

    Initial Commit

---
+++ day-1/puzzle1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class puzzle1
+{
+    public static int depthIncreaseCount(String filename) throws IOException
+    {
+
+    }
+
+    public static void main (String [] args) throws IOException
+    {
+
+    }
+}
